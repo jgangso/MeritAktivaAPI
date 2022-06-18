8602ef120297426e9475fa4624c7c5ac436e8a89
Johannes Gangsö <jgangso@gmail.com>
2022-06-18 22:52:33 +0300

    Remove non-standard tests

--- tests/inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function ($className)
-{
-	$className = str_replace('Infira\MeritAktiva\\', '', $className);
-	include_once "../src/" . $className . '.php';
-});
-$apiID  = 'place your api ID here';
-$apiKey = 'place your apy key here';
-
